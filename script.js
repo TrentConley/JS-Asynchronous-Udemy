5e07a30223b5193ec141c988585ed1b0917c929f
TrentConley <trentconley@gmail.com>
2022-03-18 10:58:11 -0500

    learning AJAX

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 const request = new XMLHttpRequest();
+const country = "peru";
+request.open("GET", `https://restcountries.com/v3.1/name/{${country}}`);
+request.send();
