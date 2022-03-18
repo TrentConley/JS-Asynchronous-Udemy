706008ae48fa157f4120753cdae76ac92e083a62
TrentConley <trentconley@gmail.com>
2022-03-18 11:01:00 -0500

    learning AJAX

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ const request = new XMLHttpRequest();
 const country = "peru";
 request.open("GET", `https://restcountries.com/v3.1/name/{${country}}`);
 request.send();
+
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText); //parsing returns an array of one object, so we destructure
+  console.log(data);
+}); // waiting for event of data loading
