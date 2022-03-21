3d94df7f6009e8bbee1303c684878f9b7daee6e0
TrentConley <trentconley@gmail.com>
2022-03-21 16:43:45 -0500

    learning AJAX

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 console.log(`hello world`);
 
 const whereAmI = function (lat, lng) {
-  //do stuff
+  //create url
+  const url = `https://geocode.xyz/${lat},${lng}?geoit=json`;
+  fetch(url).then(response => {
+    //  turn response into workable data, handle errors
+    // use data to look at attributes in console
+    // then print something like "You are in Franklin, Tennessee" to the console.
+    // chain a .catch method to the end of the promise and log any errors
+  });
 };
