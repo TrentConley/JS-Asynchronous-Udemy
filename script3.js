3fb05d70162afdf7a236452daf7ceabd0773af19
TrentConley <trentconley@gmail.com>
2022-03-22 16:19:56 -0500

    learning promises

--- script3.js
+++ script3.js
@@ -1,2 +1,11 @@
 "using strict";
 //simple lottery game to practice how promises work
+
+//promise takes in executor function, executor function takes in resolve and reject functions
+const lotteryPromise = new Promise(function (resolve, reject) {
+  if (Math.random() >= 0.5) {
+    resolve("you win!"); //value we pass into resolve function will be value available for then method.
+  } else {
+    reject("you lose!");
+  }
+});
