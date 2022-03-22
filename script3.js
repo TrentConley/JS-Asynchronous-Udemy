91facf11c02d2c45c2bae56dd61b297eaf77b9a8
TrentConley <trentconley@gmail.com>
2022-03-22 16:24:44 -0500

    learning promises

--- script3.js
+++ script3.js
@@ -3,12 +3,18 @@
 
 //promise takes in executor function, executor function takes in resolve and reject functions
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) {
-    resolve("you win!"); //value we pass into resolve function will be value available for then method.
-  } else {
-    reject("you lose!");
-  }
+  console.log(`lottery draw happening!`);
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("you win!"); //value we pass into resolve function will be value available for then method.
+    } else {
+      reject("you lose!");
+    }
+  }, 3000);
 });
 lotteryPromise
   .then(resultion => console.log(resultion))
   .catch(err => console.log(err));
+console.log("hi there");
+
+//promisifying --> wrap up old callback behavior to promise behavior
